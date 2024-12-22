6c6788bb799884ae321b83caabe1f5e8d012ef31
Денис <denis.k0walenko@yandex.ru>
2024-12-22 17:36:35 +0200

    added grid header

---
+++ grid.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Grid {
+   private:
+	int num_rows;
+	int num_colomns;
+	int cell_size;
+
+   public:
+	Grid();
+};
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-
+		ClearBackground(darkBlue);
 		EndDrawing();
 	}
 	CloseWindow();
