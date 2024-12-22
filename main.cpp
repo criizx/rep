317f75acb7fbd59185986e6d19a59301ced430f0
Денис <denis.k0walenko@yandex.ru>
2024-12-22 17:27:57 +0200

    added .clang

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 
 int main() {
-  std::cout << "starting" << std::endl;
-  const int screenHeight = 1030, screenWidth = 1920;
-  InitWindow(screenWidth, screenHeight, "tetris");
-  SetTargetFPS(60);
-  while (!WindowShouldClose()) {
-  }
-  CloseWindow();
+	Color darkBlue = {44, 44, 127, 255};
+	std::cout << "starting" << std::endl;
+	const int screenHeight = 1030, screenWidth = 1920;
+	InitWindow(screenWidth, screenHeight, "tetris");
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		EndDrawing();
+	}
+	CloseWindow();
 }
